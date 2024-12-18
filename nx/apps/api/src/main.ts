a4dea5f5808ed7996e27cb9c140dcfb0fd6fa28e
RTsosssoldi <luca.bertolini@raintonic.com>
2024-12-18 10:57:35 +0100

    create dockerfile and cofigs

--- nx/apps/api/src/main.ts
+++ nx/apps/api/src/main.ts
@@ -11,8 +11,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
+  const port = process.env.NESTJS_PORT || 3000;
+  await app.listen(port, '0.0.0.0');
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
