50d54a656c3b50ed603688a462087d09432daf46
RTsosssoldi <luca.bertolini@raintonic.com>
2024-12-18 15:03:35 +0100

    feat: add tailwind and show db data inside a table

--- nx/apps/sandbox/src/app/app.component.ts
+++ nx/apps/sandbox/src/app/app.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { Observable } from 'rxjs';
 
 @Component({
-  imports: [NxWelcomeComponent, RouterModule, HttpClientModule],
+  imports: [RouterModule, HttpClientModule, CommonModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
@@ -12,16 +13,19 @@ import { NxWelcomeComponent } from './nx-welcome.component';
 export class AppComponent {
   title = 'sandbox';
 
+  todos$?: Observable<any>;
+
   constructor(private _httpSvc: HttpClient) {}
 
   tryAPI(): void {
-    this._httpSvc.get('http://localhost:3000/api').subscribe({
-      next: (res) => {
-        console.log(res);
-      },
-      error: (err) => {
-        console.log(err);
-      },
-    });
+    this.todos$ = this._httpSvc.get('http://localhost:3000/api');
+    // this._httpSvc.get('http://localhost:3000/api').subscribe({
+    //   next: (res) => {
+    //     console.log(res);
+    //   },
+    //   error: (err) => {
+    //     console.log(err);
+    //   },
+    // });
   }
 }
